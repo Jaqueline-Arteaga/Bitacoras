502d33537b75d61ae62711569ab5364dd171d12f
Pedro Emmanuel Sanchez Petul <pedro.petul@ciencias.unam.mx>
2023-02-15 20:09:25 -0600

    Casi muero

--- bitacora-frontend/src/app/app.module.ts
+++ bitacora-frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -13,6 +12,7 @@ import { ProjectsComponent } from './pages/projects/projects.component';
 import { ActivityoverviewComponent } from './components/activityoverview/activityoverview.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ActivityComponent } from './pages/activity/activity.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
